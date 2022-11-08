b9ecdc77a0570827d705c9c23802aa34f6dfa271
Urusov Artur <urusovartur110@gmail.com>
2022-11-09 01:11:26 +0300

    Initial commit. Относительно оригинального репозитория были доработаны классы User(Переопределен toString), UserDaoJDBCImpl(Реализованы все методы согласно задаче), Util(реализована логика соединения) и UserServiceIlpl(Реализованы все методы согласно задаче)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Artur", "Urusov", (byte)22);
+        userService.saveUser("Rutra", "sobaken", (byte)33);
+        userService.saveUser("Test", "testtt", (byte)23);
+        userService.saveUser("asdasd", "asdasd", (byte)123);
+        List<User> allUsers = userService.getAllUsers();
+        for (User i : allUsers) {
+            System.out.println(i);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    static private String URL = "jdbc:mysql://localhost:3306/pp_1_1_3-4?autoReconnect=true&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static private String USERNAME = "root";
+    static private String PASSWORD = "root";
+    static public Connection getConnetion() throws SQLException {
+        Driver driver = new com.mysql.cj.jdbc.Driver();
+        DriverManager.registerDriver(driver);
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return connection;
+    }
+
+
 }
